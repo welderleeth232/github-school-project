a60688c3ad95bd3e395bcbf01a780ff1bdccf940
welderleeth232 <welderleeth232@gmail.com>
2025-03-22 17:15:27 +0000

    Add 18.ts

---
+++ src/18.ts
@@ -0,0 +1,4 @@
+// This is an example of a simple TypeScript function that takes two numbers as input and returns their sum.
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
