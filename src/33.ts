a4927565ff0d1253791595b56da9758f9e5d3da3
welderleeth232 <welderleeth232@gmail.com>
2025-04-23 10:24:20 +0000

    Add 33.ts

---
+++ src/33.ts
@@ -0,0 +1,9 @@
+function calculateSum(numbers: number[]): number {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+
+console.log(calculateSum([1, 2, 3, 4, 5]));
