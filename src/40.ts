71fdc647b5ecb48a5626999ba065fb82c00b9b84
welderleeth232 <welderleeth232@gmail.com>
2025-05-20 10:00:38 +0000

    Add 40.ts

---
+++ src/40.ts
@@ -0,0 +1,4 @@
+// This is an example of a simple TypeScript program.
+// It includes a variable to store some information and a main function to run it.
+let message = "Hello, world!";
+console.log(message);
